38e11dfab547a9a78a40b1b6a6463fb747e5385f
yyqxpyylaopo <100391472+yyqxpyylaopo@users.noreply.github.com>
2022-05-31 21:47:37 +0800

    Update common.js

--- public/js/common.js
+++ public/js/common.js
@@ -1,13 +1,13 @@
-var type="pc";  //当前属于移动端还是PC端，小于等于1000为移动端
-var pomodoroTime = 300;  //默认任务时间5分钟，即300秒
-var taskTimer = null,stopTimer=null; //taskTimer任务的计时，即Pomodoro的计时，stopTimer 正计时的计时，即stop watch的计时
-//根据屏幕宽度计算设置相对应的根元素font-size,从而更改rem大小
+var type="pc";  //Check whether the current device belongs to the mobile terminal or PC terminal. The value is less than or equal to 1000.
+var pomodoroTime = 300;  //The default task duration is 5 minutes.
+var taskTimer = null,stopTimer=null; //TaskTimer Timing of a task, namely Pomodoro timing.
+//Change the REM size by calculating the corresponding root element font-size based on the screen width.
 ; (function (win) {
     var tid
     function refreshRem() {
         let html = document.documentElement
-        let wW = html.clientWidth // 窗口宽度
-        let designSize = 1000 // 设计图尺寸
+        let wW = html.clientWidth // Window width
+        let designSize = 1000 // Design dimension
         if(wW<=1000){
             designSize=370
             type="mobile"
@@ -38,16 +38,17 @@ var taskTimer = null,stopTimer=null; //taskTimer任务的计时，即Pomodoro的
     refreshRem();
 
 })(window)
-//判断某节点是否隐藏
+//Check whether a node is hidden
 function isNone(dom){
     let res = dom.css("display")=='none'?true:false;
     return res;
 }
-//秒转换成时分秒   type为h返回时分秒，为其他返回分秒
-function formatSeconds(second,type){//秒转换成时分秒
-    var s = parseInt(second);// 需要转换的时间秒 
-    var m = 0;// 分 
-    var h = 0;// 小时 
+
+function formatSeconds(second,type){//Seconds convert to hours and minutes and seconds
+
+    var s = parseInt(second);//Time required to convert in seconds
+    var m = 0;//minute
+    var h = 0;//hour 
     if (s > 60) {
         m = parseInt(s / 60);
         s = parseInt(s % 60);
